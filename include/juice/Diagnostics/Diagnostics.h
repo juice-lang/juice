b405c48d5f8ff22281ac8e9cb2878d249428af5f
Josef Zoller <josef@walterzollerpiano.com>
2019-04-16 22:04:55 +0200

    [WIP] Add DiagnosticID enum
    The DiagnosticID enum holds all IDs of diagnostic strings, defined in
    the Diagnostics.def file.

---
+++ include/juice/Diagnostics/Diagnostics.h
@@ -0,0 +1,26 @@
+// include/juice/Diagnostics/Diagnostics.h - DiagnosticEngine class, manages any diagnostics emitted by juice
+//
+// This source file is part of the juice open source project
+//
+// Copyright (c) 2019 juice project authors
+// Licensed under MIT License
+//
+// See https://github.com/juice-lang/juice/blob/master/LICENSE for license information
+// See https://github.com/juice-lang/juice/blob/master/CONTRIBUTORS.txt for the list of juice project authors
+
+
+#ifndef JUICE_DIAGNOSTICS_H
+#define JUICE_DIAGNOSTICS_H
+
+#include <cstdint>
+
+namespace juice {
+    namespace diag {
+        enum class DiagnosticID: uint32_t {
+            #define DIAG(KIND, ID, Text) ID,
+            #include "Diagnostics.def"
+        };
+    }
+}
+
+#endif //JUICE_DIAGNOSTICS_H
