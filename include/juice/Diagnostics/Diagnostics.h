7ff5d3800c43f3cefd15ca9a682978438bc5bdb5
Josef Zoller <josef@walterzollerpiano.com>
2019-04-16 22:17:58 +0200

    [WIP] Add DiagnosticArg class
    The DiagnosticArg class wraps the different arguments, passed to the
    diagnostic engine, for filling in a diagnostic string placeholder.

--- include/juice/Diagnostics/Diagnostics.h
+++ include/juice/Diagnostics/Diagnostics.h
@@ -13,6 +13,9 @@
 #define JUICE_DIAGNOSTICS_H
 
 #include <cstdint>
+#include <vector>
+
+#include "juice/Basic/StringRef.h"
 
 namespace juice {
     namespace diag {
@@ -24,6 +27,47 @@ namespace juice {
         enum class DiagnosticKind {
             error,
             warning
+        };
+
+        class DiagnosticArg {
+        public:
+            enum class Kind {
+                integer,
+                boolean,
+                string
+            };
+
+        private:
+            Kind _kind;
+            union {
+                int _integer = 0;
+                bool _boolean;
+                basic::StringRef _string;
+            };
+
+        public:
+            DiagnosticArg(int integer): _kind(Kind::integer), _integer(integer) {}
+            DiagnosticArg(bool boolean): _kind(Kind::boolean), _boolean(boolean) {}
+            DiagnosticArg(basic::StringRef string): _kind(Kind::string), _string(string) {}
+
+            static void getAllInto(std::vector<DiagnosticArg> & vector) {}
+
+            template<typename T>
+            static void getAllInto(std::vector<DiagnosticArg> & vector, T first) {
+                vector.push_back(first);
+            }
+
+            template<typename T, typename... Args>
+            static void getAllInto(std::vector<DiagnosticArg> & vector, T first, Args... args) {
+                vector.push_back(first);
+                getAllInto(vector, args...);
+            }
+
+            Kind getKind() const { return _kind; }
+
+            int getAsInteger() const { return _integer; }
+            bool getAsBoolean() const { return _boolean; }
+            basic::StringRef getAsString() const { return _string; }
         };
     }
 }
